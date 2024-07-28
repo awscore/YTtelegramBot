0b563999b7757c0506df13cd4f21c5c74ed430ea
awscore <artur.badakov@gmail.com>
2024-07-28 20:30:40 +0300

    add welcome add save_username

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+users = {}
+
 @bot.message_handler(commands=['start'])
 def welcome(message):
     chat_id = message.chat.id
     bot.send_message(chat_id,
-                     'Привет! Добро пожаловать, помочь скачать видео из Ютуб?')
+                     'Привет! Добро пожаловать, давай знакомиться, как тебя зовут?')
+    users[chat_id] = {}
+    bot.register_next_step_handler(message, save_username)
+
+
+def save_username(message):
+    chat_id = message.chat.id
+    name = message.text
+    users[chat_id] = name
+    bot.send_message(chat_id, f'Отлично, {name}. Теперь укажи свою фамилию')
 
 
 
